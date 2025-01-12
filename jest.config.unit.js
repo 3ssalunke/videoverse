639ca53c3ad7129012c30797740bb2597021ba8e
3ssalunke <surajsalunke1276@gmail.com>
2025-01-13 00:37:49 +0530

    13010033: update jest config for e2e test

--- jest.config.unit.js
+++ jest.config.unit.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.ts"],
+  testMatch: ["**/__tests__/**/*.test.ts", "!**/*.e2e.test.ts"],
   setupFilesAfterEnv: ["./src/test/prisma-singleton.ts"],
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.ts", "!src/**/*.test.ts"],
