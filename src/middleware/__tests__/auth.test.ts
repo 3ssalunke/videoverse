4f376590f6d6f46bdfa52ce63e27e3d2b9f83483
3ssalunke <surajsalunke1276@gmail.com>
2025-01-12 14:16:14 +0530

    12011416: middleware unit tests

---
+++ src/middleware/__tests__/auth.test.ts
@@ -0,0 +1,51 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Request, Response } from "express";
+import { authenticate } from "../auth";
+
+describe("authenticate Middleware", () => {
+  const STATIC_API_TOKEN = process.env.STATIC_API_TOKEN;
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: jest.Mock;
+
+  beforeEach(() => {
+    req = { headers: {} };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should call next when the token is valid", () => {
+    req.headers!["authorization"] = `Bearer ${STATIC_API_TOKEN}`;
+    authenticate(req as Request, res as Response, next);
+
+    expect(next).toHaveBeenCalled();
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+  });
+
+  it("should return 401 when the token is missing", () => {
+    authenticate(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: "Unauthorized" });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  it("should return 401 when the token is invalid", () => {
+    req.headers!["authorization"] = "Bearer invalid-token";
+
+    authenticate(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: "Unauthorized" });
+    expect(next).not.toHaveBeenCalled();
+  });
+});
---
+++ src/middleware/__tests__/validate-video-upload.test.ts
@@ -0,0 +1,133 @@
+import { Request, Response } from "express";
+
+describe("validateVideoUpload middleware", () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: jest.Mock;
+
+  beforeEach(() => {
+    req = {
+      body: {},
+    };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("should call next if the video upload is valid", async () => {
+    jest.mock("fluent-ffmpeg", () => {
+      const mockFfprobe = jest.fn((_, callback) => {
+        callback(null, { format: { duration: 15 } });
+      });
+      return {
+        ffprobe: mockFfprobe,
+      };
+    });
+
+    jest.mock("multer", () => {
+      const multer: any = jest.fn(() => ({
+        single: jest.fn(() => (req: any, res: any, callback: Function) => {
+          req.file = {
+            path: "./test.mp4",
+            originalname: "test.mp4",
+          };
+          callback(null);
+        }),
+      }));
+
+      multer.diskStorage = jest.fn(() => ({
+        _handleFile: jest.fn(),
+        _removeFile: jest.fn(),
+      }));
+
+      return multer;
+    });
+
+    const { validateVideoUpload } = require("../validate-video-upload");
+    validateVideoUpload(req as Request, res as Response, next);
+
+    expect(next).toHaveBeenCalled();
+    expect(req.body).toHaveProperty("video");
+    expect(req.body.video.originalname).toBe("test.mp4");
+    expect(req.body).toHaveProperty("duration", 15);
+  });
+
+  it("should return a 400 error if no file is uploaded", async () => {
+    jest.mock("fluent-ffmpeg", () => {
+      const mockFfprobe = jest.fn((_, callback) => {
+        callback(null, { format: { duration: 15 } });
+      });
+      return {
+        ffprobe: mockFfprobe,
+      };
+    });
+
+    jest.mock("multer", () => {
+      const multer: any = jest.fn(() => ({
+        single: jest.fn(() => (req: any, res: any, callback: Function) => {
+          callback(null);
+        }),
+      }));
+
+      multer.diskStorage = jest.fn(() => ({
+        _handleFile: jest.fn(),
+        _removeFile: jest.fn(),
+      }));
+
+      return multer;
+    });
+
+    const { validateVideoUpload } = require("../validate-video-upload");
+    validateVideoUpload(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "no video file uploaded",
+    });
+  });
+
+  it("should return a 400 error if video file duration exceeds allowed limit", async () => {
+    jest.mock("fluent-ffmpeg", () => {
+      const mockFfprobe = jest.fn((_, callback) => {
+        callback(null, { format: { duration: 30 } });
+      });
+      return {
+        ffprobe: mockFfprobe,
+      };
+    });
+
+    jest.mock("multer", () => {
+      const multer: any = jest.fn(() => ({
+        single: jest.fn(() => (req: any, res: any, callback: Function) => {
+          req.file = {
+            path: "./test.mp4",
+            originalname: "test.mp4",
+          };
+          callback(null);
+        }),
+      }));
+
+      multer.diskStorage = jest.fn(() => ({
+        _handleFile: jest.fn(),
+        _removeFile: jest.fn(),
+      }));
+
+      return multer;
+    });
+
+    const { validateVideoUpload } = require("../validate-video-upload");
+    validateVideoUpload(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "video duration must be between 5 and 25 seconds",
+    });
+  });
+});
